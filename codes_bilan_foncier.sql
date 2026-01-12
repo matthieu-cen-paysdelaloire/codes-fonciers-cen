d16d00cdde5e0e1a5a4fb6f575118853ecc9c0c2
matthieu-cen-paysdelaloire <m.goubert@cenpaysdelaloire.fr>
2026-01-12 13:35:59 +0100

    Suppression des schémas .cen dans le code sql

--- codes_bilan_foncier.sql
+++ codes_bilan_foncier.sql
@@ -1,14 +1,14 @@
 -- Nombre de nouveaux sites en 2025* -- 
 SELECT count(*) 
-FROM cen.sites_cen 
+FROM sites_cen 
 WHERE date_crea_site BETWEEN '2025-01-01' AND '2025-12-31'
 
 -- Nombre total de sites -- 
-SELECT count(*) FROM cen.sites_cen
+SELECT count(*) FROM sites_cen
 
 -- Nombre d'hectares supplémentaires en 2025* --
 SELECT sum(surf_parc_maitrise_ha) 
-FROM cen.parcelles_cen 
+FROM parcelles_cen 
 WHERE 
 	( 
 		date_debut_ore 	BETWEEN '2025-01-01' AND '2025-12-31' 
@@ -18,13 +18,13 @@ WHERE
 
 -- Nombre total d'hectares --
 SELECT sum(surf_parc_maitrise_ha) 
-FROM cen.parcelles_cen
+FROM parcelles_cen
 
 -- Nombre de communes concernées par les actions du Conservatoire --
 SELECT count(distinct insee_com)
-FROM cen.parcelles_cen
+FROM parcelles_cen
 
 -- Nombre d'hectares sous maîtrise d'usage (ORE ou baux emphytéoriques) --
 SELECT sum(surf_parc_maitrise_ha)
-FROM cen.parcelles_cen
+FROM parcelles_cen
 WHERE code_mfu2 IN ('O', 'L1')
