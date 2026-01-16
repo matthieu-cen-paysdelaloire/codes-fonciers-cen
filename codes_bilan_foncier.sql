cc81b51a706cf8ecb557326c3a39ad8478fa7037
matthieu-cen-paysdelaloire <m.goubert@cenpaysdelaloire.fr>
2026-01-16 14:59:53 +0100

    Update codes_bilan_foncier.sql
    Ajout de nouveaux scripts sql pour le bilan foncier du CEN

--- codes_bilan_foncier.sql
+++ codes_bilan_foncier.sql
@@ -6,6 +6,18 @@ WHERE date_crea_site BETWEEN '2025-01-01' AND '2025-12-31'
 -- Nombre total de sites -- 
 SELECT count(*) FROM sites_cen
 
+-- Nombre d'hectares supplémentaires en 2025* (hors sites militaires) --
+SELECT sum(surf_parc_maitrise_ha) 
+FROM parcelles_cen 
+WHERE 
+	( 
+		date_debut_ore 	BETWEEN '2025-01-01' AND '2025-12-31' 
+		OR 
+		date_acquisition BETWEEN '2025-01-01' AND '2025-12-31' 
+) 
+AND id_site_cen_parc NOT IN ('TORP','HTF','BREI','AUVO','FONT')
+	
+	
 -- Nombre d'hectares supplémentaires en 2025* --
 SELECT sum(surf_parc_maitrise_ha) 
 FROM parcelles_cen 
@@ -16,6 +28,11 @@ WHERE
 		date_acquisition BETWEEN '2025-01-01' AND '2025-12-31' 
 );
 
+-- Nombre total d'hectares (hors sites militaires) --
+SELECT sum(surf_parc_maitrise_ha)
+FROM parcelles_cen
+WHERE id_site_cen_parc NOT IN ('TORP','HTF','BREI','AUVO','FONT')
+	
 -- Nombre total d'hectares --
 SELECT sum(surf_parc_maitrise_ha) 
 FROM parcelles_cen
@@ -23,6 +40,12 @@ FROM parcelles_cen
 -- Nombre de communes concernées par les actions du Conservatoire --
 SELECT count(distinct insee_com)
 FROM parcelles_cen
+
+-- Nombre d'hectares en propriété du CEN  --
+SELECT sum(surf_parc_maitrise_ha)
+FROM parcelles_cen
+WHERE code_mfu2 = 'P1'
+
 
 -- Nombre d'hectares sous maîtrise d'usage (ORE ou baux emphytéoriques) --
 SELECT sum(surf_parc_maitrise_ha)
