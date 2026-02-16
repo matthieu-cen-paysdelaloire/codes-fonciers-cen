db5e1acf42697f23dc0427460400c95ab7230fbb
matthieu-cen-paysdelaloire <m.goubert@cenpaysdelaloire.fr>
2026-02-16 13:42:42 +0100

    filtre_années.sql
    Mise en place des conditions afin de ne garder que les données jusqu'à l'année du bilan concerné (2025 dans le cas présent).

--- codes_bilan_foncier.sql
+++ codes_bilan_foncier.sql
@@ -3,9 +3,12 @@ SELECT count(*)
 FROM sites_cen 
 WHERE date_crea_site BETWEEN '2025-01-01' AND '2025-12-31'
 
--- Nombre total de sites -- 
+	
+-- Nombre total de sites en 2025* -- 
 SELECT count(*) FROM sites_cen
+WHERE date_crea_site < '2026-01-01'
 
+	
 -- Nombre d'hectares supplémentaires en 2025* (hors sites militaires) --
 SELECT sum(surf_parc_maitrise_ha) 
 FROM parcelles_cen 
@@ -28,26 +31,32 @@ WHERE
 		date_acquisition BETWEEN '2025-01-01' AND '2025-12-31' 
 );
 
--- Nombre total d'hectares (hors sites militaires) --
+-- Nombre total d'hectares (hors sites militaires) en 2025* --
 SELECT sum(surf_parc_maitrise_ha)
 FROM parcelles_cen
 WHERE id_site_cen_parc NOT IN ('TORP','HTF','BREI','AUVO','FONT')
+	AND date_crea_site < '2026-01-01'
+
 	
--- Nombre total d'hectares --
+-- Nombre total d'hectares en 2025* --
 SELECT sum(surf_parc_maitrise_ha) 
 FROM parcelles_cen
+WHERE date_crea_site < '2026-01-01'
 
--- Nombre de communes concernées par les actions du Conservatoire --
+	
+-- Nombre de communes concernées par les actions du Conservatoire en 2025* --
 SELECT count(distinct insee_com)
 FROM parcelles_cen
+WHERE date_crea_site < '2026-01-01'
 
--- Nombre d'hectares en propriété du CEN  --
+	
+-- Nombre d'hectares en propriété du CEN  en 2025* --
 SELECT sum(surf_parc_maitrise_ha)
 FROM parcelles_cen
-WHERE code_mfu2 = 'P1'
+WHERE code_mfu2 = 'P1' AND date_crea_site < '2026-01-01'
 
 
--- Nombre d'hectares sous maîtrise d'usage (ORE ou baux emphytéoriques) --
+-- Nombre d'hectares sous maîtrise d'usage (ORE ou baux emphytéoriques) en 2025* --
 SELECT sum(surf_parc_maitrise_ha)
 FROM parcelles_cen
-WHERE code_mfu2 IN ('O', 'L1')
+WHERE code_mfu2 IN ('O', 'L1') AND date_crea_site < '2026-01-01'
